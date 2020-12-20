6a29a07e953bdcc6f590d33c38732e8f7816d878
CareeriaTommiAL <tommi.lipponen@student.careeria.fi>
2020-12-20 12:12:01 +0200

    Lisättiin SqLite ja EF Core Nugetit, AppDbContext ja Names- luokka. Konfiguroitiin tietokantayhteys Startup-luokan ConfigureServices dependency injection metodiin.

---
+++ NameApp/Server/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NameApp.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NameApp.Server.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Name> Names { get; set; }
+    }
+}
--- NameApp/Server/Startup.cs
+++ NameApp/Server/Startup.cs
@@ -2,9 +2,11 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using NameApp.Server.Data;
 using System.Linq;
 
 namespace NameApp.Server
@@ -17,12 +19,11 @@ namespace NameApp.Server
         }
 
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+      
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<AppDbContext>(options =>
+              options.UseSqlite(Configuration.GetConnectionString("Kanta")));
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
