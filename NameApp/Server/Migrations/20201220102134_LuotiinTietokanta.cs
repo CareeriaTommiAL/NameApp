da6c7bccf11ea4ad50bc0610313df6e6629a4fdb
CareeriaTommiAL <tommi.lipponen@student.careeria.fi>
2020-12-20 12:33:03 +0200

    Luotiin NimiDb Tietokanta.

---
+++ NameApp/Server/Migrations/20201220102134_LuotiinTietokanta.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NameApp.Server.Migrations
+{
+    public partial class LuotiinTietokanta : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Names",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nimi = table.Column<string>(type: "TEXT", nullable: true),
+                    Amount = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Names", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Names");
+        }
+    }
+}
