32c0612a57db482b44d9023a622c42092e768165
CareeriaTommiAL <tommi.lipponen@student.careeria.fi>
2020-12-25 10:22:16 +0200

    Lisättiin data-annotations Name- luokkaan ja vastaavat validointi toiminnot AddName sivuun.

--- NameApp/Shared/Name.cs
+++ NameApp/Shared/Name.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,13 @@ namespace NameApp.Shared
     public class Name
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Nimi vaaditaan.")]
+        [MinLength(2, ErrorMessage = "Nimessä pitää olla vähintään kaksi merkkiä.")]
+        [MaxLength(16, ErrorMessage = "Nimessä saa olla korkeintaan 16 merkkiä.")]
         public string Nimi { get; set; }
+
+        [Range(1, 1000, ErrorMessage = "Nimiä voi lisätä 1 - 1000 kerralla.")]
         public int Amount { get; set; }
     }
 }
